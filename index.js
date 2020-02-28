7eb46c8151b1b877ae7fecc6cd87911387b8c272
evantilton <evantilton@gmail.com>
2020-02-28 17:54:07 -0600

    commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello world")
+let CronJob = require('cron').CronJob;
+const { exec } = require('child_process');
+
+let job = new CronJob('10 * * * * *', function() {
+    console.log("hello")
+    // yourscript;
+    // commit;
+  }, null, true, 'America/Los_Angeles');
+  job.start();
+
+
+
+let yourscript = exec('git add .',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
+
+let commit = exec('git commit -m "commit"',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
