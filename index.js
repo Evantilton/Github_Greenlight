6bb74d6ca7d163a21224a43090a84094e65f4df5
evantilton <evantilton@gmail.com>
2020-02-28 18:38:23 -0600

    made some progress, got it to display properly, got cronjob to work at 10 seconds, and got it to execute a terminal command

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let CronJob = require('cron').CronJob;
 const { exec } = require('child_process');
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
 
-// let job = new CronJob('10 * * * *', function() {
-//     console.log("hello")
-//   }, null, true, 'America/Los_Angeles');
-//   job.start();
+let job = new CronJob('*/10 * * * * *', function() {
+    pretty();
+  }, null, true, 'America/Los_Angeles');
+  job.start();
 
 // let save = exec('git add .',
 //   (error, stdout, stderr) => {
@@ -15,9 +18,18 @@ const { exec } = require('child_process');
 //       }
 //   });
 
+
+clear()
+function pretty(){
+console.log(
+    chalk.green(
+      figlet.textSync('Greenlight', { horizontalLayout: 'full' })
+    )
+  );
+    
 console.log('hello world');
 
-let gitAdd = exec('git add .',
+let gitbranch = exec('git branch',
         (error, stdout, stderr) => {
             console.log(stdout);
             console.log(stderr);
@@ -28,24 +40,26 @@ let gitAdd = exec('git add .',
 
 console.log('git add');
 
-let gitCommit = exec('git commit -m "commit commit"',
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
+// let gitCommit = exec('git commit -m "new commit test"',
+//         (error, stdout, stderr) => {
+//             console.log(stdout);
+//             console.log(stderr);
+//             if (error !== null) {
+//                 console.log(`exec error: ${error}`);
+//             }
+//         });
 
 console.log('git commit');
 
-let gitPush = exec('git push -u origin master',
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
+// let gitPush = exec('git push -u origin master',
+//         (error, stdout, stderr) => {
+//             console.log(stdout);
+//             console.log(stderr);
+//             if (error !== null) {
+//                 console.log(`exec error: ${error}`);
+//             }
+//         });
 
 console.log('git push');
+
+    }
