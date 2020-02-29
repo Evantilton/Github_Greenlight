c02e2ec1a736d506f991ed26df3cd2d6a3f9d657
evantilton <evantilton@gmail.com>
2020-02-28 22:35:25 -0600

    test test test

--- program.js
+++ program.js
@@ -7,6 +7,6 @@ f=_=>                          // define `f` as function with a single unused ar
         `${b=+b},${+a+b}`      // template string incrementing fibonacci numbers in place
     ),                         // end replace()
     console.log(               // prints to stdout, `undefined` passed to argument
-      (2584,4181)                    // comma separated group returns value of last expression
+      (6765,10946)                    // comma separated group returns value of last expression
     )                          // end console.log()
   );f()
