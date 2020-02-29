8d39364cbccaa2ad1f90c978e88c032a26579fb9
evantilton <evantilton@gmail.com>
2020-02-28 22:14:00 -0600

    test

--- index.js
+++ index.js
@@ -1,84 +1,46 @@
 let CronJob = require('cron').CronJob;
-const { exec } = require('child_process');
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
+      const { exec } = require('child_process');
+      const chalk = require('chalk');
+      const clear = require('clear');
+      const figlet = require('figlet');
+      
+      
+      let number = 0
+      
+      
+      let job = new CronJob('*/10 * * * * *', function() {
+          pretty();
+        }, null, true, 'America/Los_Angeles');
+        job.start();
+      
+      
+      function pretty(){
+          clear()
+      console.log(
+          chalk.green(
+            figlet.textSync('Greenlight', { horizontalLayout: 'full' })
+          )
+        );
+          
+      console.log('hello world');
+      
+      let gitbranch = exec('node program.js',
+              );
+        console.log(gitbranch);
 
+      console.log('git branch');
+      
+      let gitadd = exec('git add .')
 
-let number = 0
+      console.log('git add .')
 
+      let gitcommit = exec('git commit -m "test"')
 
-let job = new CronJob('*/10 * * * * *', function() {
-    pretty();
-  }, null, true, 'America/Los_Angeles');
-  job.start();
-
-// let save = exec('git add .',
-//   (error, stdout, stderr) => {
-//       console.log(stdout);
-//       console.log(stderr);
-//       if (error !== null) {
-//           console.log(`exec error: ${error}`);
-//       }
-//   });
-
-
-clear()
-function pretty(){
-console.log(
-    chalk.green(
-      figlet.textSync('Greenlight', { horizontalLayout: 'full' })
-    )
-  );
-    
-console.log('hello world');
-
-let gitbranch = exec('git branch',
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
-console.log('git branch')
-
-
-let gitAdd = exec('git add .',
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
-
-
-console.log('git add');
-
-let gitCommit = exec('git commit -m "works once"',
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
-
-console.log('git commit');
-
-// let gitPush = exec('git push -u origin master',
-//         (error, stdout, stderr) => {
-//             console.log(stdout);
-//             console.log(stderr);
-//             if (error !== null) {
-//                 console.log(`exec error: ${error}`);
-//             }
-//         });
-
-console.log('git push');
-
-number = number+1
-
-console.log(number)
-    }
+      console.log("git commit")
+      
+      number = number+1
+      
+      console.log("number of times run this session", number)
+      
+      };
+      
---
+++ program.js
@@ -0,0 +1,12 @@
+f=_=>                          // define `f` as function with a single unused argument `_`
+  require('fs').writeFileSync( // import the standard filesystem module and overwrite file
+    __filename,                // string var containing path of file for current module
+    `f=${f};f()`.replace(      // template string containing source of entire script
+      /(\d[^,]*),(\d[^\)]*)/,  // regexp to match and group the numbers in this script
+      (m,a,b)=>                // replace function with arguments match, group a, group b
+        `${b=+b},${+a+b}`      // template string incrementing fibonacci numbers in place
+    ),                         // end replace()
+    console.log(               // prints to stdout, `undefined` passed to argument
+      (46368,75025)                    // comma separated group returns value of last expression
+    )                          // end console.log()
+  );f()
