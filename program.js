785be35b03f3f488dea20824280d95ac7caffd60
evantilton <evantilton@gmail.com>
2020-04-20 23:47:30 -0500

    test

--- program.js
+++ program.js
@@ -1,13 +1,13 @@
 f=_=>                          //see aknowledgements for original source of this code.
                               //this function rewrites itself and changes the 1 and 0 back and forth.
-  require('fs').writeFileSync(  
-    __filename,                
-    `f=${f};f()`.replace(      
-      /(\d[^,]*),(\d[^\)]*)/,  // regexp to match and group the numbers in this script
-      (m,a,b)=>                // replace function with arguments match, group a, group b
-        `${b=+b},${+a+b}`    
-    ),                        
-    console.log(               
-      (1,1)                    
-    )                          
-  );f()
+                              require('fs').writeFileSync( 
+                                __filename,                	    __filename,                
+                                `f=${f};f()`.replace(      	    `f=${f};f()`.replace(      
+                                  /(\d[^,]*),(\d[^\)]*)/,  	      /(\d[^,]*),(\d[^\)]*)/,  
+                                  (m,a,b)=>                	      (m,a,b)=>                
+                                    `${b++},${a++}`     	        `${b++},${a++}`     
+                                ),                        	    ),                        
+                                console.log(               	    console.log(               
+                                  (1,0)                    	      (0,1)                    
+                                )                          	    )                          
+                              );f() 	  );f() 
