b3f47eade730efc78bf090d2e902c2512c3007b5
evantilton <evantilton@gmail.com>
2020-04-21 01:12:03 -0500

    test

--- program.js
+++ program.js
@@ -8,6 +8,6 @@ f=_=>                          //see aknowledgements for original source of this
           `${b++},${a++}`     	       
       ),                        	                   
       console.log(               	       
-        (0,1)                    	                   
+        (1,0)                    	                   
       )                          	                           
     );f()
