42249d3d0904e4db945a480e6bc2afebae084e86
evantilton <evantilton@gmail.com>
2020-04-20 23:48:30 -0500

    test

--- program.js
+++ program.js
@@ -1,13 +1,14 @@
 f=_=>                          //see aknowledgements for original source of this code.
                               //this function rewrites itself and changes the 1 and 0 back and forth.
-                              require('fs').writeFileSync( 
-                                __filename,                	    __filename,                
-                                `f=${f};f()`.replace(      	    `f=${f};f()`.replace(      
-                                  /(\d[^,]*),(\d[^\)]*)/,  	      /(\d[^,]*),(\d[^\)]*)/,  
-                                  (m,a,b)=>                	      (m,a,b)=>                
-                                    `${b++},${a++}`     	        `${b++},${a++}`     
-                                ),                        	    ),                        
-                                console.log(               	    console.log(               
-                                  (1,0)                    	      (0,1)                    
-                                )                          	    )                          
-                              );f() 	  );f() 
+    require('fs').writeFileSync( 
+      __filename,                	        
+      `f=${f};f()`.replace(      	    
+        /(\d[^,]*),(\d[^\)]*)/,  	     
+        (m,a,b)=>                	              
+          `${b++},${a++}`     	       
+      ),                        	                   
+      console.log(               	       
+        (1,0)                    	                   
+      )                          	                           
+    );f() 	 
+
